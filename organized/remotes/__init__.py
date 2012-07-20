10cd8b09bafa24b9c8f418bc5ae153cae88e1051
Paul Tagliamonte <tag@pault.ag>
2012-07-19 21:52:18 -0400

    initial import

---
+++ organized/remotes/__init__.py
@@ -0,0 +1,53 @@
+from organized.db import db
+
+import logging
+import urllib2
+import urllib
+
+logger = logging.getLogger('organized')
+
+class Remote(object):
+    def _fetch_bugs(self, project):
+        raise NotImplemented("Sorry.")
+
+    def _get_api_response(self, url):
+        f = urllib2.urlopen(url)
+        return f.read()
+
+    def _safe_urlencode(self, dic):
+        return urllib.urlencode(dic)
+
+    def update(self):
+        logger.info('updating')
+        return self._update()
+
+    def project(self, project):
+        logger.info('setting project to %s' % project)
+        self._project = project
+
+    def get_bugs_for(self, **kwargs):
+        logger.info('fetching bugs')
+        return self._fetch_bugs(**kwargs)
+
+    def get_milestones_for(self, **kwargs):
+        logger.info('fetching milestones')
+        return self._fetch_milestones(**kwargs)
+
+    def save_issue(self, issue):
+        # ok, slight mangle.
+        issue['_project'] = self._project
+        logger.info('adding issue %s' % issue['_id'])
+        db.issues.update(
+            {"_id": issue['_id']},
+            issue,
+            True,
+            safe=True)
+
+    def save_milestone(self, milestone):
+        # ok, slight mangle.
+        milestone['_project'] = self._project
+        logger.info('adding milestone %s' % milestone['_id'])
+        db.milestones.update({"_id": milestone['_id']},
+                             milestone,
+                             True,
+                             safe=True)
---
+++ organized/remotes/github.py
@@ -0,0 +1,89 @@
+#
+from organized.remotes import Remote
+import logging
+import json
+
+logger = logging.getLogger('organized')
+
+API_BASE = "https://api.github.com"
+PREFIX = "gh"
+
+class GitHub(Remote):
+
+    def __init__(self, user, repo):
+        logger.info('create github object.')
+        self.user = user
+        self.repo = repo
+
+    def _hit_api(self, url):
+        payload = json.loads(self._get_api_response(url))
+        return payload
+
+    def _fetch_bugs(self, **kwargs):
+        flags = self._safe_urlencode(kwargs)
+
+        user, repo = self.user, self.repo
+        url = "%s/repos/%s/%s/issues?%s" % (
+            API_BASE,
+            user,
+            repo,
+            flags
+        )
+        data = self._hit_api(url)
+        return data
+
+    def _fetch_milestones(self, **kwargs):
+        flags = self._safe_urlencode(kwargs)
+
+        user, repo = self.user, self.repo
+        url = "%s/repos/%s/%s/milestones?%s" % (
+            API_BASE,
+            user,
+            repo,
+            flags
+        )
+        data = self._hit_api(url)
+        return data
+
+    def _update(self):
+        logger.info("fetching open issues")
+        issues = self.get_bugs_for(state='open')
+        logger.info("fetching closed issues")
+        issues += self.get_bugs_for(state='closed')
+
+        issue_migration = {
+            "assignee": "owner",
+            "labels": "tags",
+            "user": "reporter"
+        }
+
+        for issue in issues:
+            eye_dee = "%s-issue-%s-%s-%s" % (
+                PREFIX,
+                self.user,
+                self.repo,
+                issue['number']
+            )
+
+            for mig in issue_migration:
+                issue[issue_migration[mig]] = issue[mig]
+                del(issue[mig])
+
+            issue['_id'] = eye_dee
+            self.save_issue(issue)
+
+        logger.info("fetching open milestones")
+        milestones = self.get_milestones_for(state='open')
+        logger.info("fetching closed milestones")
+        milestones += self.get_milestones_for(state='closed')
+
+        for milestone in milestones:
+            eye_dee = "%s-milestone-%s-%s-%s" % (
+                PREFIX,
+                self.user,
+                self.repo,
+                milestone['number']
+            )
+            milestone['_id'] = eye_dee
+            self.save_milestone(milestone)
+
